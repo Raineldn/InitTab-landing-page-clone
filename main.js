94ab05b2f9648c07ccafbbf549cfc64c97514890
raineldn <raineldn@gmail.com>
2019-05-25 16:25:20 +0100

    Added headings and popular post JSON

---
+++ main.js
@@ -0,0 +1,62 @@
+const title = document.querySelector('.title');
+const author = document.querySelector('.author');
+const score = document.querySelector('.score');
+
+// Popular On R/JavaScript
+const popularPosts = [
+{
+	"title": "Why I think Chingu is awesome",
+	"author": "u/Raine",
+	"score": "142"
+},
+{
+	"title": "JavaScript's for loop is surprisingly fast",
+	"author": "u/TurnaroundGames",
+	"score": "48"
+},
+{
+	"title": "great article to know about async programming in node js",
+	"author": "u/h_sa1994",
+	"score": "156"
+},
+{
+	"title": "pixi.js - The HTML5 Creation Engine: Create beautiful digital content with the fastest, most flexible 2D WebGL renderer",
+	"author": "u/magenta_placenta",
+	"score": "62"
+},
+{
+	"title": "I made a version of Gitfolio with vanilla JS",
+	"author": "u/weakN0",
+	"score": "6"
+},
+{
+	"title": "Feeling the imposter syndrome hard, need advice",
+	"author": "u/imanuglyperson",
+	"score": "15"
+},
+{
+	"title": "Webpack From Zero to Hero - Chapter 3: Everything is a Module",
+	"author": "u/rubenspgcavalcante",
+	"score": "110"
+},
+{
+	"title": "Can't understand why generators are usefull besides async operations",
+	"author": "u/neutral24",
+	"score": "39"
+},
+{
+	"title": "Strict null checking the Visual Studio Code codebase",
+	"author": "u/dwaxe",
+	"score": "110"
+},
+{
+	"title": "Snake in 100 lines of JS.",
+	"author": "u/StuckOnCoboldLevel",
+	"score": "36"
+}];
+
+
+
+
+
+
